cd6a8abb449aa10b7447da929a9b6a2438566fe6
wish Malik <malik.mehwish0078@gmail.com>
2024-04-16 14:23:00 -0700

    Created a word counter

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+// import inquirer module which is command Line Interface for Node.js
+import inquirer from "inquirer";
+// Declare a constant answers and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter Your Sentence to Count the Word!!"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+// Print the Array of words to the console
+console.log(words);
+// Print the word count of the sentence to the console
+console.log(`Your sentence word Count is ${words.length}`);
